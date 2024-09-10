3bc61cbe31123dc849adfe9ec10228c513ccd6da
WilliamAlvesrez <william.alves@estudante.iftm.edu.br>
2024-09-09 22:15:24 -0300

    5 - Methods should not be empty (java:S1186) @Test
    " indica que o método de teste contextLoads está vazio e, portanto, não está realizando nenhuma verificação ou validação. Métodos de teste devem sempre conter lógica que verifica se o código está funcionando corretamente. Um método de teste vazio não realiza nenhuma verificação, o que diminui a eficácia dos testes automatizados.
    Foi adicionado assertThat  para verifica se o bean 'Calculadora' foi carregado no contexto da aplicação.

--- demo/src/test/java/edu/iftm/br/demo/DemoApplicationTests.java
+++ demo/src/test/java/edu/iftm/br/demo/DemoApplicationTests.java
@@ -1,13 +1,19 @@
 package edu.iftm.br.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ApplicationContextTest {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private Calculadora calculadora;
 
+    @Test
+    void contextLoads() {
+       
+        assertThat(calculadora).isNotNull();
+    }
 }
